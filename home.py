72ae598e5bbfb4d7f6f9e24aba173c69f92b0dad
Dolan <dolankumarbairagi@gmail.com>
2019-10-29 14:53:13 +0600

    all templates are added into project

--- home.py
+++ home.py
@@ -6,7 +6,66 @@ homereq = Blueprint('home', __name__)
 
 @homereq.route('/home')
 def home():
-    return render_template('home.html')
+    try:
+        return render_template('home.html')
+    except TemplateNotFound:
+        abort(404)
+
+
+@homereq.route('/home/post-job')
+def go_to_post_job():
+    try:
+        return render_template('new-post.html')
+    except TemplateNotFound:
+        abort(404)
+
+
+@homereq.route('/home/Jobs')
+def job_post():
+    try:
+        return render_template('job-post.html')
+    except TemplateNotFound:
+        abort(404)
+
+
+@homereq.route('/home/Job-Details')
+def job_detail():
+    try:
+        return render_template('blog-single.html')
+    except TemplateNotFound:
+        abort(404)
+
+
+@homereq.route('/home/contact')
+def contact():
+    try:
+        return render_template('contact.html')
+    except TemplateNotFound:
+        abort(404)
+
+
+@homereq.route('/home/blog')
+def blog():
+    try:
+        return render_template('blog.html')
+    except TemplateNotFound:
+        abort(404)
+
+
+@homereq.route('/home/blog-details')
+def blog_details():
+    try:
+        return render_template('blog-single.html')
+    except TemplateNotFound:
+        abort(404)
+
+
+@homereq.route('/home/about')
+def about():
+    try:
+        return render_template('blog-single.html')
+    except TemplateNotFound:
+        abort(404)
 
 
 homereq.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
